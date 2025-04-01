227e1af3af99fdeb14dc7c2ded263207ae5402b3
ArtemYakimets <artemyakimets0110@gmail.com>
2025-04-02 00:00:42 +0300

    Add flush cache

--- main.cpp
+++ main.cpp
@@ -8,6 +8,15 @@
 #include "sqlite3_select/sqlite3_select.h"
 #include "generate_string/generate_string.h"
 
+void flush_cache() {
+    constexpr size_t cache_size = 32 * 1024 * 1024; // 32 GB
+    static std::vector<char> dummy(cache_size, 0);
+
+    for (volatile char &c : dummy) {
+        c += 1; 
+    }
+}
+
 int main() {
 
     for (int k = 100000; k <= 1000000; k += 100000) {
@@ -16,6 +25,7 @@ int main() {
 
         std::vector<std::string> strings_vector = generate_strings(3000, k);
         
+        flush_cache();
         std::cout << "Start insertion in database" << std::endl;
         auto start_insertion_db = std::chrono::high_resolution_clock::now();
         write_strings_to_db("strings.db", strings_vector);
@@ -23,6 +33,7 @@ int main() {
         std::chrono::duration<double> duration_insertion_db = end_insertion_db - start_insertion_db;
         std::cout << "Database insertion time: " << duration_insertion_db.count() << " seconds" << std::endl;
 
+        flush_cache();
         std::cout << "Start insertion in file" << std::endl;
         auto start_insertion_file = std::chrono::high_resolution_clock::now();
         write_strings_to_file("strings.txt", strings_vector);
@@ -30,6 +41,7 @@ int main() {
         std::chrono::duration<double> duration_insertion_file = end_insertion_file - start_insertion_file;
         std::cout << "File insertion time: " << duration_insertion_file.count() << " seconds" << std::endl;
 
+        flush_cache();
         std::cout << "Start reading from database" << std::endl;
         auto start_reading_db = std::chrono::high_resolution_clock::now();
         std::vector<std::string> strings_db = read_strings_from_db("strings.db");
@@ -37,6 +49,7 @@ int main() {
         std::chrono::duration<double> duration_reading_db = end_reading_db - start_reading_db;
         std::cout << "Database reading time: " << duration_reading_db.count() << " seconds" << std::endl;
 
+        flush_cache();
         std::cout << "Start reading from file" << std::endl;
         auto start_reading_file = std::chrono::high_resolution_clock::now();
         std::vector<std::string> strings_file = read_strings_from_file("strings.txt");
@@ -46,6 +59,7 @@ int main() {
 
         std::string search_substring = "abc"; 
 
+        flush_cache();
         std::cout << "Start selection from database" << std::endl;
         auto start_selection_db = std::chrono::high_resolution_clock::now();
         std::vector<std::string> filtered_strings_db = read_filtered_strings_from_db("strings.db", search_substring);
@@ -53,6 +67,7 @@ int main() {
         std::chrono::duration<double> duration_selection_db = end_selection_db - start_selection_db;
         std::cout << "Database selection time: " << duration_selection_db.count() << " seconds" << std::endl;
         
+        flush_cache();
         std::cout << "Start selection from file" << std::endl;
         auto start_selection_file = std::chrono::high_resolution_clock::now();
         std::vector<std::string> filtered_strings_file = read_filtered_strings_from_file("strings.db", search_substring);
